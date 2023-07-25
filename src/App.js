6b6604e9bc38a1ecd08f48118eaeefc4fc3f4bd8
David <davidngacha01@gmail.com>
2023-07-25 15:32:42 +0300

    Edit App js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import Aos from "aos";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/UI/Hero";
+import "./App.css";
+import Services from "./components/UI/Services";
+import Portfolio from "./components/UI/portfolio";
+import Contact from "./components/UI/Contact";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
+  return<>
+  <Header/>
+  <main>
+   <Hero />
+   <Services />
+   <Portfolio />
+   <Contact />
+  </main>
+  <Footer/>
+  </>
 }
 
 export default App;
